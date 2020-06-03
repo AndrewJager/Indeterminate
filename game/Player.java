b0d9d878b6544a2a751b1f9b16a3788330ee4295
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2020-06-02 22:37:16 -0400

    Remove usages of old animation framework

--- game/Player.java
+++ game/Player.java
@@ -16,14 +16,14 @@ import java.util.Map;
 
 import framework.GameObject;
 import framework.Image;
-import framework.Joint;
 import framework.KeyMapping;
 import framework.KeyState;
 import framework.Level;
-import framework.Sprite;
-import framework.SpriteSheet;
 import framework.Wall;
 import framework.WallTypes;
+import framework.animation.Frame;
+import framework.animation.Joint;
+import framework.animation.SpriteSheet;
 import framework.PhysicsType;
 import framework.imageFilters.ImageFilter;
 import framework.imageFilters.LightenFrom;
@@ -41,7 +41,7 @@ public class Player extends GameObject{
 		r_lower_arm, l_lower_arm, r_hand, l_hand, r_hip, l_hip, r_upper_leg, l_upper_leg, r_knee, l_knee,
 		r_lower_leg, l_lower_leg, r_foot, l_foot;
 	private List<Joint> joints;
-	private Sprite one, two, three, four, five;
+	private Frame one, two, three, four, five;
 	private SpriteSheet sheet;
 	private Rectangle2D floorCheck, rCheck, lCheck; 
 	private List<Integer> jumpMotion; // List of y velocities for jumping
@@ -87,130 +87,8 @@ public class Player extends GameObject{
 		h = 10 * s;
 		rCheck = new Rectangle2D.Double(x + xOffset - (w / 2), y + yOffset - (h / 2), w, h);
 		lCheck = new Rectangle2D.Double(x - xOffset - (w / 2), y + yOffset - (h / 2), w, h);
-		
-		//Idle
-		one = setToBaseSprite();
-		
-		// Walking
-		r_shoulder.rotate(35);
-		l_shoulder.rotate(-30);
-		r_elbow.rotate(-34);
-		l_elbow.rotate(-45);
-		head.rotate(5);
-		r_hip.rotate(-5);
-		r_knee.rotate(5);
-		l_hip.rotate(10);
-		l_knee.rotate(20);
-		two = new Sprite(joints);
-		
-		r_shoulder.rotate(-80);
-		l_shoulder.rotate(60);
-		head.rotate(-5);
-		r_hip.rotate(16);
-		r_knee.rotate(20);
-		l_hip.rotate(-26);
-		l_knee.rotate(-22);
-		three = new Sprite(joints);
-		
-		four = two.getFlipped(this.x);
-		five = three.getFlipped(this.x);
-		sheet = new SpriteSheet("test", two, three);
-		
-		animations.put(PlayerStates.WALK_R, sheet);
-		
-		sheet = new SpriteSheet("Test", four, five);
-		animations.put(PlayerStates.WALK_L, sheet);
-		sheet = new SpriteSheet("Test", one);
-		animations.put(PlayerStates.IDLE_R, sheet);
-		animations.put(PlayerStates.IDLE_L, sheet);
-		state = PlayerStates.WALK_R;
 	}
 	
-	/**
-	 * Sets all joints to starting position, and returns a new sprite
-	 * @return Sprite
-	 */
-	public Sprite setToBaseSprite()
-	{
-		// create the joints
-		body = new Joint(this.x, this.y, level);
-		head = new Joint(body, 0, -15);
-		r_shoulder = new Joint(body, 0, -10);
-		l_shoulder = new Joint(body, 0, -10);
-		l_shoulder.setRenderOrder(-1);
-		r_upper_arm = new Joint(r_shoulder, 0, 3);
-		l_upper_arm = new Joint(l_shoulder, 0, 3);
-		r_elbow = new Joint(r_shoulder, 0, 6);
-		l_elbow = new Joint(l_shoulder, 0, 6);
-		r_lower_arm = new Joint(r_elbow, 0, 3);
-		l_lower_arm = new Joint(l_elbow, 0 ,3);
-		r_hand = new Joint(r_elbow, 0, 6);
-		l_hand = new Joint(l_elbow, 0 ,6);
-		r_hip = new Joint(body, 0, 10);
-		l_hip = new Joint(body, 0, 10);
-		l_hip.setRenderOrder(-1);
-		r_upper_leg = new Joint(r_hip, 0, 4);
-		l_upper_leg = new Joint(l_hip, 0, 4);
-		r_knee = new Joint(r_hip, 0, 8);
-		l_knee = new Joint(l_hip, 0, 8);
-		r_lower_leg = new Joint(r_knee, 0, 4);
-		l_lower_leg = new Joint(l_knee, 0 ,4);
-		r_foot = new Joint(r_knee, 0, 8);
-		l_foot = new Joint(l_knee, 0, 8);
-		
-		// images
-		Path2D myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0, 0, 4, 20), true);
-		img = new Image(myShape, Color.BLACK, level);
-		body.addImage(img);
-		
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,5,8), true);
-		img = new Image(myShape, Color.BLACK, level);
-		img.addFilter(new LightenFrom(ShadeDir.TOP, 6));
-		head.addImage(img);
-		
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,3,6), true);
-		img = new Image(myShape, Color.BLUE, level);
-		r_upper_arm.addImage(img);
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,3,6), true);
-		img = new Image(myShape, Color.GREEN, level);
-		l_upper_arm.addImage(img);
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,3,6), true);
-		img = new Image(myShape, Color.BLUE, level);
-		r_lower_arm.addImage(img);
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,3,6), true);
-		img = new Image(myShape, Color.GREEN, level);
-		l_lower_arm.addImage(img);
-		
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,4,8), true);
-		img = new Image(myShape, Color.BLUE, level);
-		r_upper_leg.addImage(img);
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,4,8), true);
-		img = new Image(myShape, Color.GREEN, level);
-		l_upper_leg.addImage(img);
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,3,6), true);
-		img = new Image(myShape, Color.BLUE, level);
-		r_lower_leg.addImage(img);
-		myShape = new Path2D.Double();
-		myShape.append(new Rectangle2D.Double(0,0,3,6), true);
-		img = new Image(myShape, Color.GREEN, level);
-		l_lower_leg.addImage(img);
-		
-		
-		joints = new ArrayList<Joint>();
-		Sprite BaseSprite = new Sprite(joints, body, head, r_shoulder, l_shoulder, r_upper_arm, l_upper_arm, 
-				r_elbow, l_elbow, r_lower_arm, l_lower_arm, r_hand, l_hand, r_hip, l_hip, r_upper_leg,
-				l_upper_leg, r_knee, l_knee, r_lower_leg, l_lower_leg, r_foot, l_foot);
-		return BaseSprite;
-	}
 	@Override
 	public void rescale()
 	{
@@ -229,7 +107,7 @@ public class Player extends GameObject{
 	public void update(double dt, KeyState keys) {
 		int xVel = 0;
 		int yVel = 0;
-		animations.get(state).update(keys);
+//		animations.get(state).update(dt, keys);
 		idle = false;
 		if (keys.right_key)
 		{
@@ -272,7 +150,7 @@ public class Player extends GameObject{
 		{
 			state = targetState;
 			targetState = null;
-			animations.get(state).reset();
+//			animations.get(state).reset();
 		}
 		switch(state)
 		{
@@ -378,7 +256,7 @@ public class Player extends GameObject{
 
 	@Override
 	public void render(Graphics2D g2d, boolean debug) {
-		animations.get(state).render(g2d, debug);
+//		animations.get(state).render(g2d, debug);
 		if (debug)
 		{
 			g2d.setColor(new Color(155, 155, 155, 155));
@@ -391,15 +269,15 @@ public class Player extends GameObject{
 	{
 		this.x += xVel;
 		this.y += yVel;
-		for (SpriteSheet sheet : animations.values()) {
-			sheet.setTranslated(false);
-		}
-		for (SpriteSheet sheet : animations.values()) {
-			if (!sheet.getTranslated())
-			{
-				sheet.translate((int)xVel, (int)yVel);
-			}
-		}
+//		for (SpriteSheet sheet : animations.values()) {
+//			sheet.setTranslated(false);
+//		}
+//		for (SpriteSheet sheet : animations.values()) {
+//			if (!sheet.getTranslated())
+//			{
+//				sheet.translate((int)xVel, (int)yVel);
+//			}
+//		}
 		floorCheck = new Rectangle2D.Double(floorCheck.getX() + xVel, floorCheck.getY() + yVel, floorCheck.getWidth(), floorCheck.getHeight());
 		rCheck = new Rectangle2D.Double(rCheck.getX() + xVel, rCheck.getY() + yVel, rCheck.getWidth(), rCheck.getHeight());
 		lCheck = new Rectangle2D.Double(lCheck.getX() + xVel, lCheck.getY() + yVel, lCheck.getWidth(), lCheck.getHeight());
